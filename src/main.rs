75c6d31e862209a40cdf73b78164176e3b87d556
Alien-Mega <redouanebh4@gmail.com>
2023-11-29 19:02:26 +0100

    set the structure of the project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,87 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::net::{TcpListener, TcpStream};
+use std::io::prelude::*;
+
+struct Resquest {
+	buff: String	
+}
+
+impl Resquest
+{
+	fn new(buff: String) -> Resquest {
+		Resquest { buff }
+	}
+}
+
+struct Response {
+	header: String,
+	body: String
+}
+
+impl Response
+{
+	fn new(header: String, body: String) -> Response {
+		Response {
+			header,
+			body
+		}
+	}
+}
+
+fn read_request(mut stream: &TcpStream) -> Resquest
+{
+	let mut buf = [0; 1024];
+
+	Resquest::new(|| -> String {
+		stream.read(&mut buf).unwrap();	
+		buf[0].to_string()
+	}())
+}
+
+fn send_response(mut stream: &TcpStream, response: Response)
+{
+	let buff = format!(
+		"{}\r\nContent-Length: {}\r\n\r\n{}",
+		response.header,
+		response.body.len(),
+		response.body
+	);
+	stream.write(buff.as_bytes()).unwrap();
+	stream.flush().unwrap();
+}
+
+fn handle_connection(mut stream: TcpStream)
+{
+	let request = read_request(&stream);
+
+	let header = "HTTP/1.1 200 OK".to_string();
+	let body_path = "index.html";
+	let body = fs::read_to_string(body_path).unwrap();
+
+	send_response(&stream, Response::new (
+		header,
+		body
+	))
+}
+
+fn setup() -> TcpListener
+{
+	TcpListener::bind("127.0.0.1:1337").unwrap()
+}
+
+fn event_loop(server: TcpListener)
+{
+	for connection in server.incoming()
+	{
+		let stream = connection.unwrap();
+	
+		handle_connection(stream);
+	}
+}
+
+fn main()
+{
+	let server = setup();
+
+	event_loop(server);
 }
