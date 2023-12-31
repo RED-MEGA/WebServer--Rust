f8898d2346e2c500281898e9da91f24744e555b4
reben-ha <redouanebh@gmail.com>
2023-12-31 10:12:49 -0800

    Refactor Request module

--- src/request.rs
+++ src/request.rs
@@ -2,32 +2,34 @@ use crate::methods::Methods;
 use std::{io::Read, net::TcpStream};
 
 pub struct Request {
-    pub buff: String,
     pub method: Methods,
-    pub protocol: String,
+    pub http_version: String,
 }
 
 impl Request {
     pub fn new(buff: String) -> Result<Request, String> {
-        let request = match buff.lines().next() {
-            Some(line) => line,
+
+        let method: Methods;
+        let request: Vec<&str>;
+
+        request = match buff.lines().next() {
+            Some(line) => line.split(' ').collect(),
             None => return Err("Empty request".to_owned()),
         };
 
-        let request: Vec<&str> = request.split(' ').collect();
-
+        /* Debug  */
         println!("<\n");
         for elm in &request {
             println!("{}\n", elm);
         }
         println!("\n>");
-
-
+        
         eprintln!("<\n");
         eprintln!("{}", buff);
         eprintln!("\n>");
+        /* Debug  */
 
-        let method = match request[0] {
+        method = match request[0] {
             "GET" => Methods::GET(request[1].to_owned()),
             "POST" => Methods::POST(request[1].to_owned()),
             "DELETE" => Methods::DELETE(request[1].to_owned()),
@@ -35,9 +37,8 @@ impl Request {
         };
 
         Ok(Request {
-            buff: buff.clone(),
             method,
-            protocol: request[2].to_string(),
+            http_version: request[2].to_string(),
         })
     }
 }
